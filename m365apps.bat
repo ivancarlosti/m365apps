d09361f17cf1a2b8a1ac89b47e4e60b35326b18f
Ivan Carlos <ivan@ivancarlos.com.br>
2026-05-16 18:10:14 -0300

    Fix path again

--- m365apps.bat
+++ m365apps.bat
@@ -1,13 +1,14 @@
 @echo off
 setlocal
-:: Force the script to run from the folder where it is saved
+:: Force the script to run from the folder where it is saved.
+:: setup.exe will be downloaded here, alongside the XMLFiles\ folder,
+:: so we can use relative paths when invoking it.
 cd /d "%~dp0"
+:: Keep UTF-8 codepage so latin characters render correctly.
+chcp 65001 >nul
 cls
 
-:: Absolute, fully-resolved paths. Avoids any CWD or codepage surprises.
-set "SCRIPT_DIR=%~dp0"
-set "XML_DIR=%SCRIPT_DIR%XMLFiles"
-set "SETUP_EXE=%SCRIPT_DIR%setup.exe"
+set "SETUP_EXE=setup.exe"
 set "SETUP_URL=https://officecdn.microsoft.com/pr/wsus/setup.exe"
 
 :: --- Download the latest Office Deployment Tool setup.exe -------------------
@@ -28,10 +29,10 @@ echo Download complete.
 echo.
 
 :: --- Sanity check: XMLFiles folder must sit next to this .bat ---------------
-if not exist "%XML_DIR%\" (
+if not exist "XMLFiles\" (
     echo.
-    echo ERROR: The XMLFiles folder was not found.
-    echo Expected location: %XML_DIR%
+    echo ERROR: The XMLFiles folder was not found in:
+    echo   %cd%
     echo Make sure you ran m365apps.bat from inside the m365apps folder
     echo and that the XMLFiles folder is present.
     pause
@@ -59,16 +60,16 @@ echo Please press the number of your choice...
 choice /c 1234567890 /n /m "Selection: "
 set "op=%errorlevel%"
 
-if "%op%"=="1"  call :run "InstallOfficeProjectVisio-Business.xml"   "Office Business + Project + Visio" & goto end
-if "%op%"=="2"  call :run "InstallOffice-Business-US.xml"            "Office Business (US+BR+MX)"        & goto end
-if "%op%"=="3"  call :run "InstallOffice-Business-BR.xml"            "Office Business (Brazil)"          & goto end
+if "%op%"=="1"  call :run "InstallOfficeProjectVisio-Business.xml"   "Office Business + Project + Visio"   & goto end
+if "%op%"=="2"  call :run "InstallOffice-Business-US.xml"            "Office Business (US+BR+MX)"          & goto end
+if "%op%"=="3"  call :run "InstallOffice-Business-BR.xml"            "Office Business (Brazil)"            & goto end
 if "%op%"=="4"  call :run "InstallOfficeProjectVisio-Enterprise.xml" "Office Enterprise + Project + Visio" & goto end
-if "%op%"=="5"  call :run "InstallOffice-Enterprise-US.xml"          "Office Enterprise (US+BR+MX)"      & goto end
-if "%op%"=="6"  call :run "InstallOffice-Enterprise-BR.xml"          "Office Enterprise (Brazil)"        & goto end
-if "%op%"=="7"  call :run "InstallOfficeProjectVisio-Home.xml"       "Office Home + Project + Visio"     & goto end
-if "%op%"=="8"  call :run "InstallOffice-Home-US.xml"                "Office Home (US+BR+MX)"            & goto end
-if "%op%"=="9"  call :run "InstallOffice-Home-BR.xml"                "Office Home (Brazil)"              & goto end
-if "%op%"=="10" call :run "UninstallOffice.xml"                       "Uninstall Office"                  & goto end
+if "%op%"=="5"  call :run "InstallOffice-Enterprise-US.xml"          "Office Enterprise (US+BR+MX)"        & goto end
+if "%op%"=="6"  call :run "InstallOffice-Enterprise-BR.xml"          "Office Enterprise (Brazil)"          & goto end
+if "%op%"=="7"  call :run "InstallOfficeProjectVisio-Home.xml"       "Office Home + Project + Visio"       & goto end
+if "%op%"=="8"  call :run "InstallOffice-Home-US.xml"                "Office Home (US+BR+MX)"              & goto end
+if "%op%"=="9"  call :run "InstallOffice-Home-BR.xml"                "Office Home (Brazil)"                & goto end
+if "%op%"=="10" call :run "UninstallOffice.xml"                      "Uninstall Office"                    & goto end
 
 :end
 echo.
@@ -77,18 +78,19 @@ echo Process finished. Please check if the Office Setup started.
 pause
 exit /b 0
 
-:: --- Subroutine: validate XML, then invoke setup.exe ------------------------
+:: --- Subroutine: validate XML, then invoke setup.exe with a relative path ---
 :run
-set "XML=%XML_DIR%\%~1"
+set "XML_REL=XMLFiles\%~1"
 echo.
 echo === %~2 ===
-echo Config file: %XML%
-if not exist "%XML%" (
+echo Working directory: %cd%
+echo Config file (relative): %XML_REL%
+if not exist "%XML_REL%" (
     echo.
-    echo ERROR: Configuration file not found at the path above.
-    echo Make sure XMLFiles\%~1 exists next to m365apps.bat.
+    echo ERROR: Configuration file not found:
+    echo   %cd%\%XML_REL%
     goto :eof
 )
 echo Running setup.exe...
-"%SETUP_EXE%" /configure "%XML%"
+.\setup.exe /configure "%XML_REL%"
 goto :eof
